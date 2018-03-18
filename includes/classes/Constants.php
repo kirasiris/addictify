8896cf175ce4d27c15f500a98be4a6030ce23b04
Kevin Uriel Fonseca <kebin1421@hotmail.com>
2018-03-18 00:39:40 -0500

    Add files via upload

---
+++ includes/classes/Constants.php
@@ -0,0 +1,18 @@
+<?php
+class Constants {
+
+	public static $passwordsDoNoMatch = "Tu password no concuerda";
+	public static $passwordNotAlphanumeric = "Tu password unicamente puede contener numeros y letras";
+	public static $passwordCharacters = "Tu password dee ser entre 5 y 30 caracteres";
+	public static $emailInvalid = "Email es invalido";
+	public static $emailsDoNotMatch = "Tus emails no concuerdan";
+	public static $emailTaken = "Este email ya esta en uso";
+	public static $lastNameCharacters = "Tu segundo nombre debe ser entre 2 y 25 caracteres";
+	public static $firstNameCharacters = "Tu primer nombre debe ser entre 2 y 25 caracteres";
+	public static $usernameCharacters = "Tu nombre de usuario debe ser entre 2 y 25 caracteres";
+	public static $usernameTaken = "Este nombre de usuario ya existe";
+
+	public static $loginFailed = "Tu nombre de usuario o password son incorrectos";
+
+}
+?>
